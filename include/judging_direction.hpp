8cd7e50dec4ae56989d117caabc6aec72a77952c
is-whale <2966365639@qq.com>
2023-11-14 18:16:43 +0800

    feat : finish direction judeging and pid

--- include/judging_direction.hpp
+++ include/judging_direction.hpp
@@ -1,27 +1,27 @@
 #ifndef JUDGING_DIRECTION_H
 #define JUDGING_DIRECTION_H
+#include <pid_lib.hpp>
 
 class Judging_Direction
 {
 private:
-    /* data */
+    float angle_vel_pid_out_{0.0};
+    float angle_pid_out_{0.0};
+    float speed_pid_out_{0.0};
 
 public:
     Judging_Direction(/* args */);
-    ~Judging_Direction();
-
+    ~Judging_Direction() = default;
+    void Calc_All_Side_Deviation(void);
+    void Calc_One_Side_Deviation(uint8_t *heading_deviation, float *deviation, const float *deviation_parameter);
+    void Calc_Chassis_Motors_deviation(void);
+    std::shared_ptr<PID> car_pid_ptr_;
 // const uint8_t LINE_VALUE_ = 0; ///< 方向判断
 #define LINE_VALUE_ 0
 
-
 };
 
-Judging_Direction::Judging_Direction(/* args */)
-{
-}
-
-Judging_Direction::~Judging_Direction()
-{
-}
+// Judging_Direction::Judging_Direction() {}
+// Judging_Direction::~Judging_Direction() {}
 
 #endif
--- src/judge_direction.cpp
+++ src/judge_direction.cpp
@@ -1,8 +1,11 @@
 #include <judging_direction.hpp>
 
 #include <iostream>
-#include <ros/ros.h>
 #include <glog/logging.h>
+#include <ros/ros.h>
+#include <autoware_msgs/VehicleCmd.h>
+#include <autoware_msgs/ControlCommand.h>
+
 #include <pid_lib.hpp>
 
 static uint8_t front_grayscale_sensor[8] = {0};
@@ -15,14 +18,14 @@ static float front_deviation = 0.0f;///< 偏左为负数
 static float back_deviation = 0.0f;///<偏右为正
 
 ///< 前后轮方向偏移
-static float steer_deviation_output = 0.0f;
+static float steer_deviation_out = 0.0f;
 static float back_motors_deviation_output = 0.0f;
 
 ///< 前轮、后轮偏移Pid
 // static Pid_Position_t front_motors_deviation_pid = NEW_POSITION_PID(2990, 0, 0, 10, 10000, 0, 1000, 500);
- PID::Pid_Position_t front_motors_deviation_pid;
 // static Pid_Position_t back_motors_deviation_pid = NEW_POSITION_PID(800, 0, 0, 10, 10000, 0, 1000, 500);
-void Calc_One_Side_Deviation(uint8_t *heading_deviation, float *deviation, const float *deviation_parameter)
+
+void Judging_Direction :: Calc_One_Side_Deviation(uint8_t *heading_deviation, float *deviation, const float *deviation_parameter)
 {
    float left_deviation = 0.0f;
    float right_deviation = 0.0f;
@@ -50,16 +53,15 @@ void Calc_One_Side_Deviation(uint8_t *heading_deviation, float *deviation, const
    *deviation = left_deviation + right_deviation;
 }
 
-void Calc_All_Side_Deviation(void)
+void Judging_Direction::Calc_All_Side_Deviation(void)
 {
 	Calc_One_Side_Deviation(front_grayscale_sensor, &front_deviation, front_deviation_parameter);
-	Calc_One_Side_Deviation(back_grayscale_sensor, &back_deviation, back_deviation_parameter);
+	// Calc_One_Side_Deviation(back_grayscale_sensor, &back_deviation, back_deviation_parameter);
 }
 
-void Calc_Chassis_Motors_deviation(void)
+void Judging_Direction::Calc_Chassis_Motors_deviation(void)
 {
-	steer_deviation_output = PID::Pid_Position_Calc(0, front_deviation);
-
+	// front_motors_deviation_out = Pid_Position_Calc(&front_motors_deviation_pid, 0, front_deviation);
 	// back_motors_deviation_output = Pid_Position_Calc(&back_motors_deviation_pid, 0, back_deviation);
-    // std::cout << "zhuanwan " << steer_deviation_output << std::endl;
+	// Console.print("zhuanwan%.2f \r\n", front_motors_deviation_output);
 }
