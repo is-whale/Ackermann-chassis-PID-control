ce6d74191afcafaa517bc590a842bc730e6d0256
is-whale <2966365639@qq.com>
2023-11-20 10:37:32 +0800

    fix:修复cmd发送,检测PID数据有效性; bug : cmd发送目前在odomcallback,发送频率为50左右,是movebase的十倍

--- src/ackermann_pid_pursuit.cpp
+++ src/ackermann_pid_pursuit.cpp
@@ -1,14 +1,14 @@
 #include <ackermann_pid_pursuit.hpp>
 
 #include <cmath>
-#include <tf/transform_datatypes.h>
 
-#include <nav_msgs/Odometry.h>
-#include "geometry_msgs/PoseStamped.h"
+#include <ros/ros.h>
 #include <tf/tf.h>
 #include "tf2_ros/transform_listener.h"
-#include <ros/ros.h>
 #include "tf2_geometry_msgs/tf2_geometry_msgs.h"
+#include <nav_msgs/Odometry.h>
+#include "geometry_msgs/PoseStamped.h"
+#include <tf/transform_datatypes.h>
 
 #include <pid_lib.hpp>
 
@@ -26,18 +26,15 @@ geometry_msgs::TwistStamped currtwist;
 geometry_msgs::PoseStamped newpose;
 geometry_msgs::TwistStamped newtwist;
 
-void pid_calc_and_pub();
 geometry_msgs::Twist pub_vel;
 
 // waypoints
 std::vector<float> r_x_;
 std::vector<float> r_y_;
 
-<<<<<<< HEAD
-u_int64_t pointNum = 0; // 保存路径点的个数
+int pointNum = 0; // 保存路径点的个数
 int targetIndex = pointNum - 1;
 
-=======
 namespace cpprobotics
 {
 
@@ -54,62 +51,61 @@ std::vector<double> waypoint_orientationz;
 std::vector<double> waypoint_orientationw;
 
 // pid用的第一个路径角度
-std::array<float, 3> angle_for_pid;
+std::array<float, 3> angle_from_path;
 std::array<float, 3> angle_from_odom;
-int pointNum = 0; // 保存路径点的个数
-int targetIndex = pointNum - 1;
 
-ros::Publisher  cmd_vel_pub_;
+ros::Publisher cmd_vel_pub_;
 
 ros::Time last_time; // 用于延时
 
 struct PID_param_c
 {
- double kp;
- double ki;
- double kd;
- double last_error;
- double error;
- double integral;
- double derivative;
- double output_limit;
- bool use_integral_limit;
- bool use_output_limit;
- // 构造函数
- PID_param_c(double kp, double ki, double kd, double output_limit)
- : kp(kp),
-   ki(ki),
-   kd(kd),
-   last_error(0.0),
-   error(0.0),
-   integral(0.0),
-   derivative(0.0),
-   output_limit(output_limit),
-   use_integral_limit(false),
-   use_output_limit(true)
- {
- }
+    double kp;
+    double ki;
+    double kd;
+    double last_error;
+    double error;
+    double integral;
+    double derivative;
+    double output_limit;
+    bool use_integral_limit;
+    bool use_output_limit;
+    // 构造函数
+    PID_param_c(double kp, double ki, double kd, double output_limit)
+        : kp(kp),
+          ki(ki),
+          kd(kd),
+          last_error(0.0),
+          error(0.0),
+          integral(0.0),
+          derivative(0.0),
+          output_limit(output_limit),
+          use_integral_limit(false),
+          use_output_limit(true)
+    {
+    }
 };
 // 定义PID参数结构体指针
-PID_param_c* pid_vel = new PID_param_c(1.0, 0.1, 0.01, 1.0);
+PID_param_c *pid_vel = new PID_param_c(0.8, 0.0, 0.0, 1.0);
 /**
  * @brief PID计算函数，输入参数为目标值，实际值，PID参数结构体，返回类型是解算完成的值
- *   */ 
+ *   */
 double PID_calculate(double target, double actual, PID_param_c &pid)
 {
-double error = target - actual;
-  pid.error = error;
-  pid.integral += error;
-  pid.derivative = pid.error - pid.last_error;
-  pid.last_error = error;
-  double output = pid.kp * pid.error + pid.ki * pid.integral + pid.kd * pid.derivative;
-  if (output > pid.output_limit) output = pid.output_limit;
-  if (output < pid.output_limit) output = pid.output_limit;
-  double actual_output = pid.output_limit + output - pid.output_limit;
-  return actual_output;
+    double error = target - actual;
+    pid.error = error;
+    pid.integral += error;
+    pid.derivative = pid.error - pid.last_error;
+    pid.last_error = error;
+    double output = pid.kp * pid.error + pid.ki * pid.integral + pid.kd * pid.derivative;
+    if (output > pid.output_limit)
+        output = pid.output_limit;
+    if (output < -pid.output_limit)
+        output = -pid.output_limit;
+    double actual_output = pid.output_limit + output - pid.output_limit;
+    return actual_output;
 }
 
->>>>>>> 5f9b1a0 (ok)
 Ackermann_pid_pursuit::Ackermann_pid_pursuit(ros::NodeHandle nh) : remote_(false), unlock_(false), safe_(false), max_deque_size_(3)
 {
     bool get_param = true;
@@ -123,9 +119,7 @@ Ackermann_pid_pursuit::Ackermann_pid_pursuit(ros::NodeHandle nh) : remote_(false
         // TODO:预留给参数获取
     }
 
-    // path_sub_ = nh.subscribe<nav_msgs::Path>("/path", 10, &Ackermann_pid_pursuit::pathCallback, this);
     geometry_sub_ = nh.subscribe<geometry_msgs::PoseStamped>("/current_pose", 1, &Ackermann_pid_pursuit::poseCallback, this);
-    // cmd_vel_pub_ = nh.advertise<geometry_msgs::Twist>("/cmd_vel", 1);
 }
 /**
  * @brief 接受定位消息并且进行TF转换 -> map
@@ -144,21 +138,6 @@ void odomCallback(const nav_msgs::Odometry &odominfo)
     currtwist.header.frame_id = "odom";
     currtwist.twist = curr_vel;
 
-    // check
-    //  std ::cout << "x : %f" << curr_pose.orientation.x << "y : %f" << curr_pose.orientation.y << "z : %f" << curr_pose.orientation.z << " w : %f" << curr_pose.orientation.w << std::endl;
-
-    // 不用转了
-    //  try
-    //  {
-    //      newpose = buf.transform(currpose, "map");
-    //      // newtwist = ;
-    //      // ROS_INFO("TRANSFORM SUCC!");
-    //      // posepub_.publish(newpose);
-    //  }
-    //  catch (const std::exception &e)
-    //  {
-    //      // ROS_INFO("error %s", e.what());
-    //  }
     auto currentQuaternionX = curr_pose.orientation.x;
     auto currentQuaternionY = curr_pose.orientation.y;
     auto currentQuaternionZ = curr_pose.orientation.z;
@@ -170,11 +149,17 @@ void odomCallback(const nav_msgs::Odometry &odominfo)
     angle_from_odom =
         calQuaternionToEuler(currentQuaternionX, currentQuaternionY,
                              currentQuaternionZ, currentQuaternionW);
-    std::cout << "car_yaw: " << angle_from_odom[0] << " car_pitch: " << angle_from_odom[1] << " car_roll: " << angle_from_odom[2] << std::endl;
-    pid_calc_and_pub();
+
+    std::cout << "car_roll: " << angle_from_odom[0] << " car_pitch: " << angle_from_odom[1] << " car_yaw: " << angle_from_odom[2] << std::endl;
+    pub_vel.angular.z = PID_calculate(angle_from_path[2], angle_from_odom[2], *pid_vel);
+    pub_vel.linear.x = 0.5;
     cmd_vel_pub_.publish(pub_vel);
 
+//TODO:cmd 发布频率有点快,需要移动到其他位置或者使用定时器回调
     // std::cout << "pos1: " << curr_pose.position.x << " pos1: " << curr_pose.position.y << " pos1: " << curr_pose.position.z << std::endl;
+
+    // check origin data.
+    //  std::cout << "tar : " << angle_for_pid[2] << " pos1: " << angle_from_odom [2] << std::endl;
 }
 
 /**
@@ -232,9 +217,8 @@ void Ackermann_pid_pursuit::spin()
  */
 void path_callback(const nav_msgs::Path &msg)
 {
-    ROS_INFO("received the path,ready to judge.");
+    // ROS_INFO("received the path,ready to judge.");
     path_recive_and_direction.pathCallback(msg);
-    // pointCallback(msg);
     //    ROS_INFO("got a plan!");
 
     int pn = msg.poses.size();
@@ -248,10 +232,8 @@ void path_callback(const nav_msgs::Path &msg)
             r_x_.push_back(msg.poses[i].pose.position.x);
             r_y_.push_back(msg.poses[i].pose.position.y);
         }
-        ROS_INFO("current path len: %d", pointNum);
+        // ROS_INFO("current path len: %d", pointNum);
     }
-
-    // auto a = msg.poses[0].pose.position.x;
 
     //  ROS_INFO("point %d:%f,%f",i,msg.poses[i].pose.position.x,msg.poses[i].pose.position.y);
     else if (!msg.poses.empty() && !r_x_.empty())
@@ -273,76 +255,32 @@ void path_callback(const nav_msgs::Path &msg)
     auto currentQuaternionZ = msg.poses.data()->pose.orientation.z;
     auto currentQuaternionW = msg.poses.data()->pose.orientation.w;
 
-    std::array<float, 3> calRPY =
-        calQuaternionToEuler(currentQuaternionX, currentQuaternionY,
-                             currentQuaternionZ, currentQuaternionW);
-    std::cout << "yaw: " << calRPY[0] << " pitch: " << calRPY[1] << " roll: " << calRPY[2] << std::endl;
+    // std::array<float, 3> calRPY =
+    //     calQuaternionToEuler(currentQuaternionX, currentQuaternionY,
+    //                          currentQuaternionZ, currentQuaternionW);
+    angle_from_path = calQuaternionToEuler(currentQuaternionX, currentQuaternionY,
+                                           currentQuaternionZ, currentQuaternionW);
 
-    // path_recive_and_direction.
-
-    // path_data_size_已经存了
-    //  if (!path_recive_and_direction.getSubPath())
-    //  {
-    //  path_tmp = path_recive_and_direction.getSubPath();
-    // }
+    std::cout << "path_roll: " << angle_from_path[0] << " path_pitch: " << angle_from_path[1] << " path_yaw: " << angle_from_path[2] << std::endl;
 }
 
-/**
- * @brief 初始化话题订阅对象
- */
-/*void odom_callback(const nav_msgs::Odometry &msg)
-{
-    
-    pub_vel.angular.z = PID_calculate(angle_for_pid[2] , angle_for_pid[2], *pid_vel);
-    pub_vel.linear.x = 1;
-}
 int main(int argc, char **argv)
 {
     ros::init(argc, argv, "ackermann_pid_pursuit");
     ros::NodeHandle nh;
     Ackermann_pid_pursuit ackermann_pid_pursuit(nh);
-    // void odom_callback(const nav_msgs::Odometry &odominfo);
 
     // 暂时使用全局
     ros::Subscriber splinePath = nh.subscribe("/move_base/TebLocalPlannerROS/local_plan", 20, path_callback);
     ros::Subscriber odomMsgs = nh.subscribe("/odom", 20, odomCallback);
     cmd_vel_pub_ = nh.advertise<geometry_msgs::Twist>("/cmd_vel", 1);
 
-    // fix
-    //  ros::Subscriber odom_sub = nh.subscribe<geometry_msgs::PoseStamped>("/odom", 1, odomCallback);
-
     while (ros::ok())
     {
+        ros::spinOnce(); // 必须保留
         float min_distance = 1.0;
         int closest_index = 0;
         geometry_msgs::PoseStamped currpose;
-
-        // std::cout << "path_size " << ackermann_pid_pursuit.path_data_size_ << std::endl;
-        // std::cout << "path_size " << pointNum << std::endl;
-
-        // std::cout << "path_size " << path_tmp->poses.size() << std::endl;
-        //  for (int i = 0; i < path_tmp->poses.size(); i++)
-        // {
-        //     geometry_msgs::PoseStamped pose_stamped = path_tmp->poses[i];
-        //     geometry_msgs::Pose pose = pose_stamped.pose;
-
-        //     // 计算路径点与机器人当前位置和方向之间的距离
-        //     float dx = pose.position.x - currpose.pose.position.x;
-        //     float dy = pose.position.y - currpose.pose.position.y;
-        //     float d_theta = atan2(std::sin(pose.orientation.z - currpose.pose.orientation.z), std::cos(pose.orientation.z - currpose.pose.orientation.z));
-        //     float distance = std::sqrt(dx * dx + dy * dy + d_theta * d_theta);
-
-        //     // 如果距离小于最小距离，则更新最小距离和最接近的点
-        //     if (distance < min_distance)
-        //     {
-        //         min_distance = distance;
-        //         closest_index = i;
-        //     }
-        // }
-        // 更新机器人的当前位置和方向(注释，因为更新在odom)
-        // currpose.pose = path_tmp->poses[closest_index].pose;
-        // currpose.header = path_tmp->header;
-        // currpose.header.frame_id = "odom";
     }
     ackermann_pid_pursuit.spin();
     return 0;
